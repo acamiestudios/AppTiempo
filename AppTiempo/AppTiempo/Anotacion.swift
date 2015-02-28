76aaf487a63441c54f02b07112b8477611fad93e
Alejandro Agudelo <alejandrounalmed@gmail.com>
2015-02-28 11:53:23 -0500

    otra cosa

---
+++ AppTiempo/AppTiempo/Anotacion.swift
@@ -0,0 +1,14 @@
+//
+//  Anotacion.swift
+//  AppTiempo
+//
+//  Created by Yakeline Agudelo on 28/02/15.
+//  Copyright (c) 2015 Alejandro Agudelo. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Anotacion: MKPointAnnotation {
+   
+}
