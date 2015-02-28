ab4f4b4fefc4bdc5a997e880e48d341575422387
Yakeline Agudelo <yakeline@122.0.17.175.adsl-pool.jlccptt.net.cn>
2015-02-28 10:36:22 -0500

    Primer commit

---
+++ AppTiempo/AppTiempo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppTiempo
+//
+//  Created by Yakeline Agudelo on 28/02/15.
+//  Copyright (c) 2015 Alejandro Agudelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
